21ff3a185c4306a7174545068d046322be1b3d32
iven ari <ivenari.dacruz@versia.com>
2021-11-05 09:38:51 +0100

    add

--- src/main/java/ary/spring/persona/service/EstudianteService.java
+++ src/main/java/ary/spring/persona/service/EstudianteService.java
@@ -3,15 +3,21 @@ package ary.spring.persona.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import ary.spring.persona.model.Estudiante;
 import ary.spring.persona.repository.EstudianteRepository;
 
+@Service
 public class EstudianteService implements EstudianteRepository {
+	
+	@Autowired   //hacemos la inyecion de datos 
+	private EstudianteRepository estudianteRepository;
 
 	@Override
 	public List<Estudiante> findAll() {
@@ -84,12 +90,27 @@ public class EstudianteService implements EstudianteRepository {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
+	
 
 	@Override
 	public <S extends Estudiante> S save(S entity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
+   //creamos un metodo manualmente para guardar los estudiantes
+	public void guardar(Estudiante estudiante) {
+		estudianteRepository.save(estudiante);
+	}
+	
+	
+	
+	
+	
+	
 
 	@Override
 	public Optional<Estudiante> findById(Long id) {
