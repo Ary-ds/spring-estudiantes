70ffa975a3a1faec94a4a4bc808232ec98047c30
iven ari <ivenari.dacruz@versia.com>
2021-11-08 14:18:42 +0100

    En controller  agregamos la anotacion crossOrigin con la ruta del front-end el localhost del front para hacer la conexion

--- src/main/java/ary/spring/persona/controller/EstudianteController.java
+++ src/main/java/ary/spring/persona/controller/EstudianteController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import ary.spring.persona.model.Estudiante;
 import ary.spring.persona.service.EstudianteService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")  //para hacer la conexion con el front-end
 public class EstudianteController {
 	
 	@Autowired  //inyeccion de la clase 
